4f1cf5411f67f5454db9fbc9776629bd2f23ab76
Leonardo Vales <leonardo.vales@getrak.com.br>
2021-09-25 22:38:18 -0300

    Testa objeto de valor telefone

--- app/ValueObjects/Telefone.php
+++ app/ValueObjects/Telefone.php
@@ -2,12 +2,19 @@
 
 namespace App\ValueObjects;
 
+use InvalidArgumentException;
+
 class Telefone
 {
     private string $telefone;
+    private const REGEX = '/^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/';
 
     public function __construct(string $telefone)
     {
+        if (preg_match(self::REGEX, $telefone) == false) {
+            throw new InvalidArgumentException('Telefone inválido', 500);
+        }
+        
         $this->telefone = $telefone;
     }
     
